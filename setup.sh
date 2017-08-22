ea9a6f06c6084a62635ecece40ff73d325864d43
Junshoong <junshoong@gmail.com>
2017-08-22 15:29:56 +0900

    support upstart

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 DEST_DIR=
 SOURCE_FILE=./suredown.sh
+INIT=
+
+check_init()
+{
+    if [ -d /lib/systemd ]; then
+        INIT="systemd"
+    else
+        INIT="upstart"
+    fi
+}
 
 setup()
 {
@@ -12,14 +22,23 @@ setup()
     test -d $DEST_DIR || mkdir -p $DEST_DIR && cp $SOURCE_FILE ${DEST_DIR}/suredown
 
     echo "setup surdown files..."
-    mv ${DEST_DIR}/shutdown ${DEST_DIR}/shutdown_ 
-    mv ${DEST_DIR}/reboot ${DEST_DIR}/reboot_
-    mv ${DEST_DIR}/halt ${DEST_DIR}/halt_
-    mv ${DEST_DIR}/poweroff ${DEST_DIR}/poweroff_
-    ln -s ${DEST_DIR}/suredown ${DEST_DIR}/shutdown
-    ln -s ${DEST_DIR}/suredown ${DEST_DIR}/reboot
-    ln -s ${DEST_DIR}/suredown ${DEST_DIR}/halt
-    ln -s ${DEST_DIR}/suredown ${DEST_DIR}/poweroff
+    if [ "$INIT" == "systemd" ]; then
+        mv ${DEST_DIR}/shutdown ${DEST_DIR}/shutdown_ 
+        mv ${DEST_DIR}/reboot ${DEST_DIR}/reboot_
+        mv ${DEST_DIR}/halt ${DEST_DIR}/halt_
+        mv ${DEST_DIR}/poweroff ${DEST_DIR}/poweroff_
+        ln -s ${DEST_DIR}/suredown ${DEST_DIR}/shutdown
+        ln -s ${DEST_DIR}/suredown ${DEST_DIR}/reboot
+        ln -s ${DEST_DIR}/suredown ${DEST_DIR}/halt
+        ln -s ${DEST_DIR}/suredown ${DEST_DIR}/poweroff
+    elif [ "$INIT" == "upstart" ]; then
+        mv ${DEST_DIR}/shutdown ${DEST_DIR}/shutdown_ 
+        ln -s ${DEST_DIR}/suredown ${DEST_DIR}/shutdown
+    else
+        echo "this init system not supported"
+        rm ${DEST_DIR}/suredown
+        exit 1
+    fi
 
     echo "setup user script..."
     cp -r ./suredown.d /etc
@@ -35,14 +54,21 @@ remove()
     if [ -h ${DEST_DIR}/shutdown ]; then
         mv ${DEST_DIR}/shutdown_ ${DEST_DIR}/shutdown
     fi
-    if [ -h ${DEST_DIR}/reboot ]; then
-        mv ${DEST_DIR}/reboot_ ${DEST_DIR}/reboot
-    fi
-    if [ -h ${DEST_DIR}/halt ]; then
-        mv ${DEST_DIR}/halt_ ${DEST_DIR}/halt
-    fi
-    if [ -h ${DEST_DIR}/poweroff ]; then
-        mv ${DEST_DIR}/poweroff_ ${DEST_DIR}/poweroff
+    if [ "$INIT" == "systemd" ]; then
+        if [ -h ${DEST_DIR}/reboot ]; then
+            mv ${DEST_DIR}/reboot_ ${DEST_DIR}/reboot
+        fi
+        if [ -h ${DEST_DIR}/halt ]; then
+            mv ${DEST_DIR}/halt_ ${DEST_DIR}/halt
+        fi
+        if [ -h ${DEST_DIR}/poweroff ]; then
+            mv ${DEST_DIR}/poweroff_ ${DEST_DIR}/poweroff
+        fi
+    elif [ "$INIT" == "upstart" ]; then
+        :
+    else
+        echo "this init system not supported"
+        exit 1
     fi
     rm ${DEST_DIR}/suredown
 }
@@ -54,6 +80,7 @@ echo "1. setup"
 echo "2. remove"
 echo "3. cancel"
 read num
+check_init
 
 case ${num} in
     1 )
